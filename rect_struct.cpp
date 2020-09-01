695caac41906465a43ed13afd14619d399f5bc93
Jim Daehn <jdaehn@missouristate.edu>
2020-09-01 12:22:42 -0500

    LAB02 - Initial import of source files.

---
+++ rect_struct.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// This program uses a structure to hold data about a rectangle
+
+// PLACE YOUR NAME HERE
+
+// Fill in code to declare a structure named rectangle which has
+// members length, width, area, and perimeter all of which are floats
+
+int main()
+{
+	// Fill in code to define a rectangle variable named box
+
+	cout << "Enter the length of a rectangle: ";
+
+	// Fill in code to read in the length member of box
+
+	cout << "Enter the width of a rectangle: ";
+
+	// Fill in code to read in the width member of box
+
+	cout << endl << endl;
+
+	// Fill in code to compute the area member of box
+
+	// Fill in code to compute the perimeter member of box
+
+	cout << fixed << showpoint << setprecision(2);
+
+	// Fill in code to output the area with an appropriate message
+
+	// Fill in code to output the perimeter with an appropriate message
+
+	return 0;
+}
