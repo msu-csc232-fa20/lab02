cad52a3b8546b9d69be5056a47e016d0a7aeedae
Jim Daehn <jdaehn@missouristate.edu>
2020-09-01 21:19:09 -0500

    LAB02 - Modernize rect_struct.cpp.

--- rect_struct.cpp
+++ rect_struct.cpp
@@ -1,37 +1,43 @@
-#include <iostream>
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2020
+ * 
+ * @brief This program uses a structure to hold data about a rectangle.
+ * @file rect_struct.cpp
+ * @authors Jim Daehn <jdaehn@missouristate.edu>
+ *          TODO: replace this line with your name here in the above format
+ */
+
+#include <cstdlib>
 #include <iomanip>
-using namespace std;
+#include <iostream>
 
-// This program uses a structure to hold data about a rectangle
-
-// PLACE YOUR NAME HERE
-
-// Fill in code to declare a structure named rectangle which has
+// TODO: Fill in code to declare a structure named rectangle which has
 // members length, width, area, and perimeter all of which are floats
 
 int main()
 {
-	// Fill in code to define a rectangle variable named box
+	// TODO: Fill in code to define a rectangle variable named box
 
-	cout << "Enter the length of a rectangle: ";
+	std::cout << "Enter the length of a rectangle: ";
 
-	// Fill in code to read in the length member of box
+	// TODO: Fill in code to read in the length member of box
 
-	cout << "Enter the width of a rectangle: ";
+	std::cout << "Enter the width of a rectangle: ";
 
-	// Fill in code to read in the width member of box
+	// TODO: Fill in code to read in the width member of box
 
-	cout << endl << endl;
+	std::cout << std::endl << std::endl;
 
-	// Fill in code to compute the area member of box
+	// TODO: Fill in code to compute the area member of box
 
-	// Fill in code to compute the perimeter member of box
+	// TODO: Fill in code to compute the perimeter member of box
 
-	cout << fixed << showpoint << setprecision(2);
+	std::cout << std::fixed << std::showpoint << std::setprecision(2);
 
-	// Fill in code to output the area with an appropriate message
+	// TODO: Fill in code to output the area with an appropriate message
 
-	// Fill in code to output the perimeter with an appropriate message
+	// TODO: Fill in code to output the perimeter with an appropriate message
 
-	return 0;
+	return EXIT_SUCCESS;
 }
